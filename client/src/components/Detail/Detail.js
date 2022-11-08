ecad82c9ef30932128864a7c423a638fdbd4fb7d
Ednesor <edgar.funes2001@gmail.com>
2022-11-08 12:52:44 -0300

    cambios

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -71,6 +71,7 @@ export default function RecipDetail(props){
                 <h3>Defense: {pokemonDetail.defense}</h3>
                 <h3>Defense 2.0: {pokemonDetail.defense}</h3>
                 <h3>Defense 2.0: {pokemonDetail.defense}</h3>
+                <h3>Defense 2.0: {pokemonDetail.defense}</h3>
                 <h3>Speed: {pokemonDetail.speed}</h3>
                 </div>
             {
